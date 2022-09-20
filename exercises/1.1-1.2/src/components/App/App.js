8901304e76de6987aee4b036a979edef708586b8
Maxime Van Achter <maxime.vanachter@student.vinci.be>
2022-09-20 11:22:43 +0200

    1.1-1.2 : CSS style

--- exercises/1.1-1.2/src/components/App/App.js
+++ exercises/1.1-1.2/src/components/App/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <div id="myApp">
       <Header title={course}/>
       <Content part1={part1} part2={part2} part3={part3} ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
       <Total nbrTot={exercises1 + exercises2 + exercises3}/>
