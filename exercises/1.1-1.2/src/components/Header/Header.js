29b386651fc861e1ae9bccd74a54cd9407d801b2
Maxime Van Achter <maxime.vanachter@student.vinci.be>
2022-09-20 11:32:16 +0200

    1.1-1.2 : import image

--- exercises/1.1-1.2/src/components/Header/Header.js
+++ exercises/1.1-1.2/src/components/Header/Header.js
@@ -1,7 +1,9 @@
+import logoEcole from "C:/Users/20191877/Desktop/ksksksk/web3/exercises/1.1-1.2/src/img/LOGO HE VINCI.png"
 const Header = (props) => {
 
     return (
       <div>
+        <img src={logoEcole} alt="logoEcole"/>
         <h1>{props.title}</h1>
       </div>
     )
