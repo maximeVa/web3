6bfe99b2e226a3700216d9ab9497314f19897c41
Maxime Van Achter <maxime.vanachter@student.vinci.be>
2022-10-04 14:05:55 +0200

    unicafe - step4

--- exercises/module3/3.1/src/components/Statistics/Statistics.js
+++ exercises/module3/3.1/src/components/Statistics/Statistics.js
@@ -1,14 +1,26 @@
 const Statistics = ({ good, neutral, bad }) => {
+
+    const display = () => {
+        if(good == 0 && neutral == 0 && bad == 0){
+            return (<div>
+                <h2>Statistics</h2>
+                <p>No feedback given</p>
+            </div>);
+        }else{
+            return (<div>
+                <h2>Statistics</h2>
+                <p>Good {good}</p>
+                <p>Neutral {neutral}</p>
+                <p>Bad {bad}</p>
+                <p>All {good + neutral + bad}</p>
+                <p>Average {(good - bad) / (good + neutral + bad)}</p>
+                <p>Positive {(good / (good + neutral + bad)) * 100} %</p>
+            </div>);
+        }
+    };
+
   return (
-    <div>
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive {(good / (good + neutral + bad)) * 100} %</p>
-    </div>
+    display()
   );
 };
 
