0813d232c70a2ba085f783efc99e4d21143b97fc
maximeVa <maxime.vanachter@student.vinci.be>
2022-10-24 23:55:13 +0200

    ex 3.12 (pas fini, affichage liste en cas de 3 args ne fonctionne pas...)

---
+++ exercises/module6/ex3.12/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+    const password = process.argv[2]
+
+    const url = `mongodb+srv://petitbilouete2010:${password}@cluster0.8quo2rh.mongodb.net/?retryWrites=true&w=majority`
+    
+    const phoneSchema = new mongoose.Schema({
+        name: String,
+        phone: String,
+    })
+
+    const Phone = mongoose.model('Phone', phoneSchema)
+
+        mongoose
+      .connect(url)
+      .then((result) => {
+        console.log('connected')
+
+        if (process.argv.length === 3) {
+          console.log('Phonebook:')
+          Phone.find({}).then(result => {
+              result.forEach(phone => {
+                console.log(phone)
+              })
+              mongoose.connection.close()
+            })
+            process.exit(0)
+  
+        }
+      const name = process.argv[3]
+      const number = process.argv[4]
+    
+        const phone = new Phone({
+            name: name,
+            phone: number,
+        })
+        
+        return phone.save()
+      })
+      .then(() => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        return mongoose.connection.close()
+      })
+      .catch((err) => console.log(err))
+
+    
