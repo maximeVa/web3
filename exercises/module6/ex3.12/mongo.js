1bec29295c8f35436d027fab536ffb9bda71e446
Maxime Van Achter <maxime.vanachter@student.vinci.be>
2022-10-28 11:36:14 +0200

    Exo 3.12

--- exercises/module6/ex3.12/mongo.js
+++ exercises/module6/ex3.12/mongo.js
@@ -1,46 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-    const password = process.argv[2]
+const password = process.argv[2];
 
-    const url = `mongodb+srv://petitbilouete2010:${password}@cluster0.8quo2rh.mongodb.net/?retryWrites=true&w=majority`
-    
-    const phoneSchema = new mongoose.Schema({
-        name: String,
-        phone: String,
-    })
+const url = `mongodb+srv://petitbilouete2010:${password}@cluster0.8quo2rh.mongodb.net/?retryWrites=true&w=majority`;
 
-    const Phone = mongoose.model('Phone', phoneSchema)
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+});
 
-        mongoose
-      .connect(url)
-      .then((result) => {
-        console.log('connected')
+const Phone = mongoose.model("Phone", phoneSchema);
 
-        if (process.argv.length === 3) {
-          console.log('Phonebook:')
-          Phone.find({}).then(result => {
-              result.forEach(phone => {
-                console.log(phone)
-              })
-              mongoose.connection.close()
-            })
-            process.exit(0)
-  
-        }
-      const name = process.argv[3]
-      const number = process.argv[4]
-    
-        const phone = new Phone({
-            name: name,
-            phone: number,
-        })
-        
-        return phone.save()
-      })
-      .then(() => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        return mongoose.connection.close()
-      })
-      .catch((err) => console.log(err))
+mongoose
+  .connect(url)
+  .then(async() => {
+    console.log("connected");
 
-    
+    if (process.argv.length === 3) {
+      console.log("Phonebook:");
+      await Phone.find({}).then((result) => {
+        result.forEach((phone) => {
+          console.log(phone);
+        });
+        mongoose.connection.close();
+        process.exit(0);
+      });
+      
+    }
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    const phone = new Phone({
+      name: name,
+      phone: number,
+    });
+
+    return phone.save();
+  })
+  .then((data) => {
+    console.log(`added ${data.name} number ${data.phone} to phonebook`);
+    return mongoose.connection.close();
+  })
+  .catch((err) => console.log(err));
